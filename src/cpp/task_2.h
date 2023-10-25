f161ab0e154c9178fed61be5359c79a03d8e9e0d
Ayaaxd <127714070+Ayaaxd@users.noreply.github.com>
2023-10-25 23:27:55 +0500

    Update task_2.h

--- src/cpp/task_2.h
+++ src/cpp/task_2.h
@@ -1,1 +1,31 @@
+NUMBER GUESSING GAME 1-100
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+    int guess;
+    int tries;
+    
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+    
+    do{
+        cout << "Enter a num 1-100: ";
+        cin >> guess;
+        tries++;
+        
+        if(guess > num){
+            cout << "Too high!" << endl;
+        } else if(guess < num){
+            cout << "Too Low!" << endl;
+        } else{
+            cout << "You are right! Tries: " << tries << endl;
+        }
+      
+    }while(guess != num);
+    
+    return 0;
+}
