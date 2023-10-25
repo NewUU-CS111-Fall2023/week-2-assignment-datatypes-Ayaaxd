c4befb4edd9f658f4dfa1eb3f822f40b7ce1ade5
Ayaaxd <127714070+Ayaaxd@users.noreply.github.com>
2023-10-25 20:04:42 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -1,5 +1,16 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+using namespace std;
+int main() {
+    int year;
+    
+  cout << "Enter a year: ";
+  cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << year << " is a leap year." << std::endl;
+    } else {
+        cout << year << " is not a leap year." << std::endl;
+    }
+
+    return 0;
+}
