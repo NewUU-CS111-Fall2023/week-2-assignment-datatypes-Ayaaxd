46fcc78800d173cec694d3a89969239a7ad8f169
Ayaaxd <127714070+Ayaaxd@users.noreply.github.com>
2023-10-25 20:31:27 +0500

    Update task_2.h

--- src/cpp/task_2.h
+++ src/cpp/task_2.h
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int num;
+    int somenumber;
+    int tries;
+    
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+    
+    do{
+        cout << "Enter a number 1-100: ";
+        cin >> somenumber;
+        tries++;
+        
+        if(somenumber > num){
+            cout << "Too high!" << endl;
+        } else if(somenumber < num){
+            cout << "Too Low!" << endl;
+        } else{
+            cout << "You are right! Tries: " << tries << endl;
+        }
+        
+    }while(somenumber != num);
+    
+    return 0;
+}
